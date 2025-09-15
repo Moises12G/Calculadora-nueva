cb3a0ae21835b9ce65e2d7c0d8c6af8ab2fc4935
Moises Garcia <moisesgarciaurda@gmail.com>
2025-09-15 00:22:06 -0500

    Primer commit

---
+++ calculator/src/Calculator.tsx
@@ -0,0 +1,139 @@
+import './styles.css';
+import { useState , useMemo} from "react";
+import Buttons from "./components/Buttons";
+import Input from "./components/Input";
+import type { Operators } from "./types/operations"; 
+
+
+
+export default function Calculator() {
+    const operations: Operators = {
+  "+": (n1, n2) => n1 + n2,
+  "-": (n1, n2) => n1 - n2,
+  "✕" : (n1, n2) => n1 * n2,
+  "÷": (n1, n2) => n1 / n2,
+  "%": (_n1, n2) => n2 / 100,
+  "+/-": (_n1, n2) => n2 * -1,
+};
+const [input, setInput] = useState<string | null>(null);
+const [total, setTotal] = useState<number>(0);
+const [hasError, setHasError] = useState<boolean>(false);
+ const [oldInput, setOldInput] = useState<string | null>(null);
+ const [showOldInput, setShowOldInput] = useState<boolean>(false);
+ const [prevSymbol, setPrevSymbol] = useState<string | null>(null);
+ const [equalSignPressed, setEqualSignPressed] = useState<boolean>(false);
+ const currentInput: string | null = useMemo<string | null>(
+    () => (showOldInput ? oldInput : input),
+    [input, showOldInput]
+  );
+   
+   function handleButtonPress(value: string): void {
+    if (showOldInput) {
+        setShowOldInput(false);
+      }
+      const numValue: number = parseInt(value);
+      if (Number.isNaN(numValue)) {
+        handleSymbol(value);
+      } else {
+        storeNumToScreen(value);
+        if(equalSignPressed){
+          setTotal(0)
+        } 
+      }
+      if(value !== '='){
+        setEqualSignPressed((value === '+/-' && equalSignPressed || value === '%' || value === '.' && equalSignPressed))
+      }
+  }
+    function handleSymbol(symbol: string) {
+    switch (symbol) {
+      case "C":
+        clear()
+        break;
+
+      case "=":
+        setEqualSignPressed(true)
+        if(input===null || prevSymbol===null) return;
+        const finalTotal:number= calculate(
+            parseFloat(input),
+            total,
+            prevSymbol as keyof Operators
+        )
+        if(input==="0" && prevSymbol==="÷" ){
+            setHasError(true)
+            return
+        }
+           setInput(finalTotal.toString());
+           setPrevSymbol(null);
+           if(showOldInput) setShowOldInput(false);
+        break;
+      case "%":
+      case "+/-":
+        if(input === null){
+            return
+        }
+        const changedValue:number= calculate(
+            parseFloat(input),
+            total,
+            symbol
+        );
+        setInput(changedValue.toString());
+        
+        break;
+      case ".":
+        if(!input?.includes('.')){
+            storeNumToScreen(symbol)
+        }
+        break;
+      case "+":
+      case "-":
+      case "✕":
+      case "÷":
+       if (input === null || prevSymbol === null) {
+          if (prevSymbol === null) {
+            if (input !== null) setTotal(parseFloat(input));
+            prepareNextOperation(symbol);
+          }
+          return;
+        }
+        const newTotal: number = calculate(
+          parseFloat(input),
+          total,
+          prevSymbol as keyof Operators
+        );
+        setTotal(newTotal);
+        prepareNextOperation(symbol);
+        break;
+    }
+}
+function prepareNextOperation(symbol: string) {
+    setPrevSymbol(symbol);
+    setShowOldInput(true);
+    setOldInput(input);
+    setInput(null);
+  } 
+function clear() {
+    setInput(null);
+    setOldInput(null);
+    setShowOldInput(false);
+    setTotal(0);
+    setPrevSymbol(null);
+    setHasError(false)
+  }
+    function storeNumToScreen(num: string){
+   setInput(prev => prev === '0' || prev === null || equalSignPressed && input?.charAt(0) !== '.'? num : prev + num)
+  }
+  function calculate(
+    buffer: number,
+    currentTotal: number,
+    symbol: keyof Operators
+  ): number {
+    return operations[symbol](currentTotal, buffer);
+  }
+  
+    return (
+        <div className="calculator-container">
+          <Input error={hasError} input={currentInput ?? "0"} />
+          <Buttons handleOnPress={handleButtonPress} />
+        </div>
+      )
+}
